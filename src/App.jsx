da21dac5a9a92fb85d15d6cf3ecd3e26e51cc96d
Rghvs03 <raghav437@gmail.com>
2025-10-11 00:08:29 +0530

    Initial project setup with React and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-blue-600">
+        Welcome to Event Management System
+      </h1>
+    </div>
+  );
+};
+
+export default App;
