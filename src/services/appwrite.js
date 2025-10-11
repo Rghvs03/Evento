d4c026fff2b39da392f0934b0efece567799b7f8
Rghvs03 <raghav437@gmail.com>
2025-10-11 14:47:19 +0530

    commit

--- src/services/appwrite.js
+++ src/services/appwrite.js
@@ -1,9 +1,7 @@
-import { Client, Account } from "appwrite";
+import { Client } from "appwrite";
 
 const client = new Client()
-  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT) // Uses .env variable
-  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID); // Uses .env variable
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
-const account = new Account(client);
-
-export { client, account };
+export { client };
